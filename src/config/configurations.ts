03e84d930f73f821fb2090e1f51c11af88559334
Tihomir Yordanov <t.yordanovv@gmail.com>
2025-05-30 15:28:07 +0300

    database updates

--- src/config/configurations.ts
+++ src/config/configurations.ts
@@ -1,11 +1,11 @@
 export default () => ({
     port: parseInt(process.env.PORT || '3005', 10),
     database: {
-        host: process.env.DATABASE_HOST || 'localhost',
-        port: parseInt(process.env.DATABASE_PORT || '5435', 10),
-        username: process.env.DATABASE_USERNAME || 'postgres',
-        password: process.env.DATABASE_PASSWORD || 'postgres',
-        database: process.env.DATABASE_NAME || 'car_service_booking',
+        host: process.env.DB_HOST || 'localhost',
+        port: parseInt(process.env.DB_PORT || '5432', 10),
+        username: process.env.DB_USERNAME || 'postgres',
+        password: process.env.DB_PASSWORD || 'postgres',
+        database: process.env.DB_DATABASE || 'railway',
     },
     jwt: {
         secret: process.env.JWT_SECRET || 'your-secret-key-your-secret-key-your-secret-key-your-secret-key',
